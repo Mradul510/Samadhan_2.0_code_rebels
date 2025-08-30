35ec1b6bc6f801c9f35a18ecc2afcd138a622a11
Mradul Saxena <lorddragon200lord@gmail.com>
2025-08-30 23:42:31 +0530

    Day 7: State Management - Counter + Live Text Preview

---
+++ day_1_.js
@@ -0,0 +1,13 @@
+const student = {
+    name: "Mradul Saxena",
+    age: 19,
+    grade:"A"
+};
+
+function printStudentDetails(student) {
+    console.log("Student Details:");
+    console.log("Name:", student.name);
+    console.log("Age:", student.age);
+    console.log("Grade:", student.grade);
+}
+printStudentDetails(student);
